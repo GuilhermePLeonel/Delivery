dc13117a469d92504b0fe586c415d36250f35ec8
GuilhermePLeonel <guilhermepleonel1997@gmail.com>
2024-03-12 18:16:43 -0300

    installing dependencies

--- back-end/src/routers/auth.router.js
+++ back-end/src/routers/auth.router.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const loginController = require('../controllers/auth.controller');
+const express = require("express");
+const loginController = require("../controllers/auth.controller");
 
 const router = express.Router();
 
-router.post('/', (req, res) => loginController.login(req, res));
-router.put('/', loginController.login);
+router.post("/", (req, res) => loginController.login(req, res));
+router.put("/", loginController.login);
 
 module.exports = router;
